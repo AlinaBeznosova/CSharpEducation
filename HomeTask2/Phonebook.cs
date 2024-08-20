423509e83f18d77f14b30c6839a562dbd4362838
Alina Beznosova <abeznsva@gmail.com>
2024-08-20 20:09:46 +0400

    Домашнее задание 3. Phonebook

---
+++ HomeTask2/Phonebook.cs
@@ -0,0 +1,157 @@
+﻿namespace HomeTask3
+{
+    class Phonebook
+    {
+
+        private static Phonebook instance;
+        private static readonly object _lock = new object();
+        private List<Abonent> abonents;
+
+        private Phonebook()
+        {
+            abonents = new List<Abonent>();
+            LoadFromFileSync();
+        }
+
+        public static Phonebook Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Phonebook();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+
+        public List<Abonent> LoadFromFileSync()
+        {
+            if (File.Exists("phonebook.txt"))
+            {
+                string[] lines = File.ReadAllLines("phonebook.txt");
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(':');
+                    if (parts.Length == 2)
+                    {
+                        abonents.Add(new Abonent(parts[0], parts[1]));
+                    }
+                }
+
+            }
+            return abonents;
+        }
+
+        private void SaveToFileSync()
+        {
+            string[] lines = abonents.Select(a => $"{a.Name}:{a.PhoneNumber}").ToArray();
+            File.WriteAllLines("phonebook.txt", lines);
+        }
+
+        public void AddAbonent(Abonent abonentToAdd)
+        {
+
+            foreach (Abonent abonent in abonents.ToList())
+            {
+                if (abonent.PhoneNumber == abonentToAdd.PhoneNumber)
+                {
+                    Console.WriteLine("Абонент с таким номером уже существует.");
+                    Console.WriteLine();
+                    return;
+                }
+            }
+            abonents.Add(abonentToAdd);
+            SaveToFileSync();
+            Console.WriteLine("Абонент добавлен");
+            Console.WriteLine();
+        }
+
+        public void RemoveAbonent(string phoneNumber)
+        {
+            int count = 0;
+            foreach (Abonent abonentToRemove in abonents.ToList())
+            {
+                if (abonentToRemove.PhoneNumber == phoneNumber)
+                {
+                    abonents.Remove(abonentToRemove);
+                    SaveToFileSync();
+                    Console.WriteLine("Абонент удален");
+                    Console.WriteLine();
+                    count++;
+                }
+
+            }
+            if (count == 0)
+                Console.WriteLine("Абонент не найден");
+            Console.WriteLine();
+
+        }
+
+        public void GetAbonentByPhoneNumber(string phoneNumber)
+        {
+            int count = 0;
+            foreach (Abonent abonent in abonents)
+            {
+                if (abonent.PhoneNumber == phoneNumber)
+                {
+                    Console.WriteLine($"Имя: {abonent.Name}");
+                    Console.WriteLine();
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("Абонент не найден");
+                Console.WriteLine();
+            }
+
+        }
+
+        public void GetPhoneNumberByName(string name)
+        {
+            int count = 0;
+            foreach (Abonent abonent in abonents)
+            {
+                if (abonent.Name == name)
+                {
+                    Console.WriteLine($"Телефон: {abonent.PhoneNumber}");
+                    Console.WriteLine();
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("Абонент не найден");
+                Console.WriteLine();
+            }
+        }
+
+        public void OutputFile()
+        {
+            String line;
+
+            StreamReader sr = new StreamReader("phonebook.txt");
+
+            line = sr.ReadLine();
+
+            while (line != null)
+            {
+                Console.WriteLine(line);
+
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            Console.WriteLine();
+        }
+
+
+    }
+}
---
+++ HomeTask2/Program.cs
@@ -0,0 +1,83 @@
+﻿/*Должен уметь принимать от пользователя номер и имя телефона.
+Сохранять номер в файле phonebook.txt. (При завершении программы либо при добавлении).
+Вычитывать из файла сохранённые номера. (При старте программы).
+Удалять номера.
+Получать абонента по номеру телефона.
+Получать номер телефона по имени абонента.
+
+Обращение к Phonebook должно быть как к классу-одиночке.
+Внутри должна быть коллекция с абонентами.
+Для обращения с абонентами нужно завести класс Abonent. С полями «номер телефона», «имя».
+Не дать заносить уже записанного абонента.*/
+
+
+using System;
+using System.IO;
+using HomeTask3;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        Phonebook phonebook = Phonebook.Instance;
+
+        while (true)
+        {
+            Console.WriteLine("1. Добавить абонента");
+            Console.WriteLine("2. Удалить абонента");
+            Console.WriteLine("3. Найти абонента по номеру");
+            Console.WriteLine("4. Найти номер по имени");
+            Console.WriteLine("5. Вывести содержимое телефонной книги");
+            Console.WriteLine("6. Выход");
+
+            Console.Write("Выберите действие: ");
+            string choice = Console.ReadLine();
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Введите имя: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Введите номер телефона: ");
+                    string phoneNumber = Console.ReadLine();
+                    phonebook.AddAbonent(new Abonent(name, phoneNumber));
+                    break;
+
+                case "2":
+                    Console.Write("Введите номер телефона для удаления: ");
+                    string phoneNumberToRemove = Console.ReadLine();
+                    phonebook.RemoveAbonent(phoneNumberToRemove);
+                    break;
+
+                case "3":
+                    Console.Write("Введите номер телефона для поиска: ");
+                    string phoneNumberToFind = Console.ReadLine();
+                    phonebook.GetAbonentByPhoneNumber(phoneNumberToFind);
+                    break;
+
+                case "4":
+                    Console.Write("Введите имя для поиска: ");
+                    string nameToFind = Console.ReadLine();
+                    phonebook.GetPhoneNumberByName(nameToFind);
+                    break;
+
+                case "5":
+                    phonebook.OutputFile();
+                    break;
+
+                case "6":
+                    return;
+
+                default:
+                    Console.WriteLine("Некорректный ввод. Попробуйте еще раз. Введите цифру от 1 до 5");
+                    break;
+
+            }
+        }
+    }
+
+}
+
